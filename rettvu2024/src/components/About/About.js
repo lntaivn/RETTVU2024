6f1b749cd43ab103fa9fbd4aec81c6ac11700d8f
taiz <llamttai@gmail.com>
2024-01-22 15:11:16 +0700

    tai

--- rettvu2024/src/components/About/About.js
+++ rettvu2024/src/components/About/About.js
@@ -5,7 +5,6 @@ import { Carousel, Collapse } from "antd";
 import { DataStore } from "../Database";
 
 const About = () => {
-
     const database = useContext(DataStore);
     const About_content = database?.About_content;
     const About_content_topic = database?.About_content_topic;
@@ -15,40 +14,37 @@ const About = () => {
 
     useEffect(() => {
         window.scrollTo(0, 0);
-    }, [])
+    }, []);
 
     return (
         <>
-            <Carousel autoplay>
-                <div className="Slide-img">
-                    <img
-                        src="https://www.tvu.edu.vn/wp-content/uploads/2022/01/TVUXuan2022_1.jpg"
-                        alt=""
-                    />
-                </div>
-                <div className="Slide-img">
-                    <img
-                        src="https://www.tvu.edu.vn/wp-content/uploads/2021/12/anh1.jpg"
-                        alt=""
-                    />
-                </div>
-                <div className="Slide-img">
-                    <img
-                        src="https://image.bnews.vn/MediaUpload/Org/2020/06/15/trung-tam-hoc-lieu-tvu.jpg"
-                        alt=""
-                    />
-                </div>
-            </Carousel>
+            <div className="Slide">
+                <Carousel autoplay>
+                    <div className="Slide-img">
+                        <img
+                            src="https://www.tvu.edu.vn/wp-content/uploads/2022/01/TVUXuan2022_1.jpg"
+                            alt=""
+                        />
+                    </div>
+                    <div className="Slide-img">
+                        <img
+                            src="https://www.tvu.edu.vn/wp-content/uploads/2021/12/anh1.jpg"
+                            alt=""
+                        />
+                    </div>
+                    <div className="Slide-img">
+                        <img
+                            src="https://image.bnews.vn/MediaUpload/Org/2020/06/15/trung-tam-hoc-lieu-tvu.jpg"
+                            alt=""
+                        />
+                    </div>
+                </Carousel>
+                <div className="Banner_bottom">OKOKOK</div>
+            </div>
             <div className="About">
-                <div className="About_content_text" >
-                    <h2>
-                        About Tra Vinh University
-                    </h2>
-                    <p>
-                        {About_content.map(item => (
-                            item.Text
-                        ))}
-                    </p>
+                <div className="About_content_text">
+                    <h2>About Tra Vinh University</h2>
+                    <p>{About_content.map((item) => item.Text)}</p>
                 </div>
                 <div className="About_content_topics">
                     {About_content_topic.map((item) => (
@@ -73,7 +69,9 @@ const About = () => {
                 <div className="import_date">
                     {About_Important_Date.map((date, index) => (
                         <div key={index} className="About_content_layout">
-                            <div className="import_date_icon"><i className="fa-solid fa-calendar-days"></i></div>
+                            <div className="import_date_icon">
+                                <i className="fa-solid fa-calendar-days"></i>
+                            </div>
                             <div className="ACL-flex">
                                 <h2>{date.Date}</h2>
                                 <h3>{date.TitleDate}</h3>
@@ -87,23 +85,16 @@ const About = () => {
                 <h1> Keynote Speakers</h1>
             </div>
             <div className="Speakers">
-                {Speakers.map(speaker => (
+                {Speakers.map((speaker) => (
                     <div className="Speakers_item">
                         <div className="Speakers_item_img">
-                            <img
-                                src={speaker.imgSrc}
-                                alt=""
-                            />
+                            <img src={speaker.imgSrc} alt="" />
                         </div>
                         <h3>{speaker.title}</h3>
                         <span>{speaker.title}</span>
-                        <p>
-                            {speaker.topic}
-                        </p>
+                        <p>{speaker.topic}</p>
                     </div>
-                ))
-                }
-
+                ))}
             </div>
 
             <div className="Submission_Guideline_tital">
@@ -114,7 +105,12 @@ const About = () => {
                     <div key={index}>
                         <div className="Submission_Guideline_header">
                             {item.header.map((itemEvent, innerIndex) => (
-                                <p key={innerIndex} dangerouslySetInnerHTML={{ __html: itemEvent }} />
+                                <p
+                                    key={innerIndex}
+                                    dangerouslySetInnerHTML={{
+                                        __html: itemEvent,
+                                    }}
+                                />
                             ))}
                         </div>
                         <div className="Submission_Guideline_body">
