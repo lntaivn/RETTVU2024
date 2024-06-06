96bb6273d15f7748caae60ea22ed5d0a4f93517d
kakanvk <caoka5265@gmail.com>
2024-06-06 19:32:41 +0700

    update Program

--- rettvu2024/src/components/Program/Program.js
+++ rettvu2024/src/components/Program/Program.js
@@ -1,123 +1,84 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Carousel, Collapse } from 'antd';
 import "./Program.css"
 import { Link } from 'react-router-dom';
 import { DataStore } from "../Database";
+import { getConferenceProgram, getParallelSession1, getParallelSession2 } from "../../service/DataService";
 const { Panel } = Collapse;
 
-const contentStyle = {
-    height: '160px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
-  };
-
 const Program = () => {
-    const database = useContext(DataStore);
 
-    const SESSION_1 = database?.SESSION_1;
-    const SESSION_2 = database?.SESSION_2;
-    const ITEMS_LIST_ROOM = database?.ITEMS_LIST_ROOM;
+    const [conferenceProgram, setConferenceProgram] = useState([]);
+    const [parallelSession1, setParallelSession1] = useState([]);
+    const [parallelSession2, setParallelSession2] = useState([]);
+
+    const handleGetParallelSessions = async () => {
+        try {
+            const [response1, response2, response3] = await Promise.all([getConferenceProgram(), getParallelSession1(), getParallelSession2()]);
+
+            setConferenceProgram(response1.data);
+            setParallelSession1(response2.data);
+            setParallelSession2(response3.data);
+
+            console.log(response1.data);
+            console.log(response2.data);
+            console.log(response3.data);
+
+        } catch (error) {
+            console.error("Error while fetching Registration:", error);
+        }
+    };
 
     useEffect(() => {
-        window.scrollTo(0, 0);
+        handleGetParallelSessions();
     }, [])
-
-
-    const items = ITEMS_LIST_ROOM.map((item, index) => {
-        return ({
-            key: index,
-            label: (
-                <div>
-                    <span>
-                        <span style={{ color: 'red' }}>{item.label_room_sessions}</span>
-                        <span className='modify_Upto'>{item.label_room_details}</span>
-                        <br />
-                        <span><b>Session chairs:</b>{item.Session_chairs}</span>
-                    </span>
-                </div>
-            ),
-            children: (
-                <div>
-                    {
-                        item.children.map((itm, index) => {
-                            return (
-                                <ul>
-                                    <li>
-                                        <strong>{itm.time}</strong>
-                                        <br />
-                                        <strong>[Paper {itm.paper}]</strong>{itm.description}
-                                        <br />
-                                        <em>Authors:</em>{itm.author}
-                                        <br />
-                                        <em>Chairs:</em>{itm.chair}
-                                    </li>
-                                </ul>   
-                            )
-                        })
-                    }
-                </div>
-            )
-        })
-    })
 
     return (
         <div className="Program">
-            {/* <div className='program'>
-                <h1>CONFERENCE PROGRAM</h1>
-                <h2>I. Session 1, Saturday July 22nd, 2023</h2>
-                <div className="program_title_detail"><ul><li> <h2>Room:</h2> <h1>E21.105</h1> <Link to="/map">View map</Link></li></ul></div>
-                <div className="schedule">
-                    <ul>
-                        {SESSION_1.map(item => (
-                            <li>
-                                <span className="time">{item.time}</span>
-
-                                {item.event.map(itemEvent => (
-                                    <>
-                                        <span className='event' dangerouslySetInnerHTML={{ __html: itemEvent }} />
-                                    </>
-                                ))}
-
-                            </li>
-                        ))}
-
-                    </ul>
+            <div className="Submission_Guideline_tital">
+                <h1>Conference Program</h1>
+            </div>
+            {/* <h3 style={{padding: "20px", textAlign: "center"}}>Updating...</h3> */}
+            <div className="Session_Container">
+                <div>
+                    <div className="Session_item">
+                        <h2>I. {conferenceProgram[0]?.session}</h2>
+                        <h3>{conferenceProgram[0]?.date}</h3>
+                    </div>
+                    <div className="Session_timeline">
+                        {
+                            conferenceProgram[0]?.content?.map((item, index) => (
+                                <div key={index} className="Session_timeline_item">
+                                    <h3>{item.time}</h3>
+                                    <p>{item.description}</p>
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
-                <h2>
-                    II. Session 2, Saturday July 22nd, 2023
-                </h2>
-
-                <div className="schedule">
-                    <ul>
-                        {SESSION_2.map((item, index) => (
-                            <li key={index}>
-                                <span className="time">{item.time}</span>
-
-                                {item.event.map((itemEvent, index) => (
-                                    <>
-                                        <span key={index} className='event' dangerouslySetInnerHTML={{ __html: itemEvent }} />
-                                    </>
-                                ))}
-
-                            </li>
-                        ))}
-                    </ul>
+                <div>
+                    <div className="Session_item">
+                        <h2>II. {conferenceProgram[1]?.session}</h2>
+                        <h3>{conferenceProgram[1]?.date}</h3>
+                    </div>
+                    <div className="Session_timeline">
+                        {
+                            conferenceProgram[1]?.content?.map((item, index) => (
+                                <div key={index} className="Session_timeline_item">
+                                    <h3>{item.time}</h3>
+                                    <p>{item.description}</p>
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
-                <h1 className="modify_program_h1">Parallel sessions (13:00-14:00, <Link to="/map">View map</Link>)</h1>
-                <div className="program_event_click_arrow">
+            </div>
+            {/* <div>
+                <h2>Parallel sessions (13:00-14:00)</h2>
+                <div>
 
-                    <Collapse>
-                        {items.map(item => (
-                            <Panel key={item.key} header={item.label}>
-                                {item.children}
-                            </Panel>
-                        ))}
-                    </Collapse>
                 </div>
             </div> */}
-            <h3 style={{padding: "20px", textAlign: "center"}}>Updating...</h3>
         </div>
     )
 }
